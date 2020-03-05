1b1737a7a78409d2df1b1df4c6db1ec3e0359253
Sarthakshastri <61197806+Sarthakshastri@users.noreply.github.com>
2020-03-05 14:25:45 +0530

    Add files via upload

---
+++ file1.ts
@@ -0,0 +1,14 @@
+﻿interface Person {
+    Name: String;
+    age: number;
+}
+
+class Student implements Person {
+    Class: String;
+    favouriteSub: String; 
+}
+
+class School {
+    SchoolName: String;
+    Students: Array<string> = ['Muskan', 'Shreya', 'Pallavi', 'Shubhi'];
+}
